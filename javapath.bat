a0034d55f4abf3acfbf6b5bfd2aa0b7d616f38bf
Sandhya Chandrasekaran <SC00434321@techmahindra.com>
2017-07-07 16:07:24 +0530

    first commit

---
+++ javapath.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET JAVA_HOME=D:\jdk1.8.0_45
+SET MAVEN_HOME=D:\sonar\apache-maven-3.3.9\bin
+SET PATH=D:\jdk1.8.0_45\bin;.;D:\sonar\apache-maven-3.3.9\bin
+SET CLASSPATH=D:\jdk1.8.0_45\bin;.;
+set  SONAR_RUNNER_HOME=D:\source code analysis\static code analysis\OpensourceTools\sonar-pdf generation\New_version\sonar-scanner-2.5
+echo JAVA_HOME: %JAVA_HOME%
+echo PATH : %PATH%
+echo classpath : %CLASSPATH%
+
+
